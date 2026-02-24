c4b9e94f1dc2eb13a4690ecb9923a73a8b7c9eca
M4nw3l <63550247+M4nw3l@users.noreply.github.com>
2026-02-24 02:05:19 +0000

    Fix imports

--- __init__.py
+++ __init__.py
@@ -5,14 +5,8 @@ https://github.com/M4nw3l/pythonista-wkapp
 '''
 __version__ = '0.0.1'
 
-try:
-    from .wkwebview import *
-    from .wkapp import *
-except:
-    from wkwebview import *
-    from wkapp import *
-    
-  
+from .wkapp import *
+
 # mostly convenience includes for bottle
 # besides its implementation specific includes for the mako templating integration
 from bottle import (
